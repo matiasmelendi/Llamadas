954ed786231f50dacbf507ab17954e11b7693deb
Matias <mmelendi@10pines.com>
2014-10-31 16:19:30 -0300

    DummyInitializer, we do requests to this object to have necessary data

---
+++ lib/dummy_initialize.rb
@@ -0,0 +1,42 @@
+require_relative 'compania_telefonica'
+require_relative 'linea_telefonica'
+require_relative 'cod_area'
+require_relative 'cliente'
+require_relative 'util/utilities'
+require 'json'
+
+class DummyInitialize
+  attr_accessor :compania
+
+  def initialize
+    @compania= CompaniaTelefonica.new
+    compania.agregar_cliente("Memo",LineaTelefonica.new(CodArea.new(120,54),1511111111))
+    compania.agregar_cliente("EuroMemo",LineaTelefonica.new(CodArea.new(1,101),1522222222))
+    compania.cliente_realizo_llamada_a(compania.cliente_de_nombre("Memo"),compania.cliente_de_nombre("EuroMemo"),Duration.new(10))
+    compania.cliente_realizo_llamada_a(compania.cliente_de_nombre("EuroMemo"),compania.cliente_de_nombre("Memo"),Duration.new(10))
+ end
+
+
+  def meses_corrientes
+    @date= Date.today
+    anho= @date.year
+    mes= @date.month
+    meses= (1..13).collect{|nro_mes| MesDelAnio.new(anho,nro_mes)}
+    meses.select{|mes_del_anio| mes_del_anio.mes <= mes }
+  end
+
+  def mes_del_anho_actual
+    @date= Date.today
+    anho= @date.year
+    mes= @date.month
+    MesDelAnio.new(anho,mes)
+  end
+
+  def clientes
+    compania.clientes
+  end
+
+
+
+
+end
