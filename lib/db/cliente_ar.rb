37502991425d98ff626f1885ce55564df818cc9e
Matias <mmelendi@10pines.com>
2014-11-20 18:21:19 -0300

    many modifications, removed repeated column(id  == _id) now only exist id

--- lib/db/cliente_ar.rb
+++ lib/db/cliente_ar.rb
@@ -1,14 +1,20 @@
+require_relative 'llamadas_ar'
+
 class ClienteAR < ActiveRecord::Base
 
   ActiveRecord::Base.establish_connection(
       :adapter  => "sqlite3",
       :database => "/home/memonono/RubymineProjects/llamadas/lib/db/compania_ar_db.db")
 
+  self.table_name ='clientes'
+
+  #has_many :llamadas_ars
+
   ActiveRecord::Schema.define do
-    unless ActiveRecord::Base.connection.tables.include? 'cliente_ars'
-      create_table :cliente_ars do |table|
-        table.column :nombre,     :string
-        table.column :_id, :integer
+    unless ActiveRecord::Base.connection.tables.include? 'clientes'
+
+      create_table :clientes do |table|
+        table.column :nombre, :string
         table.column :numero, :integer
         table.column :cod_l, :integer
         table.column :cod_n, :integer
