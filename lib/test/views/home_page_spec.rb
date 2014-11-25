c552a7d1168edf0fa4081c038d9c2df560a2986c
Matias <mmelendi@10pines.com>
2014-11-25 16:24:54 -0300

    db interaction added(necessary to keep a good context).

--- lib/test/views/home_page_spec.rb
+++ lib/test/views/home_page_spec.rb
@@ -4,25 +4,29 @@ require '../../compania_telefonica'
 require '../../linea_telefonica'
 require '../../server'
 
+  before do
+    Company.new.borrar_clientes
+    Company.new.borrar_llamadas
+  end
 
-        def app
-          Sinatra::Application
-        end
+  def app
+    Sinatra::Application
+  end
 
-        describe 'homepage' do
-          it 'should be ok' do
-            get '/'
-            expect(last_response).to be_ok
-          end
+  describe 'homepage' do
+    it 'should be ok' do
+      get '/'
+      expect(last_response).to be_ok
+    end
 
-          it ' the page´s body should contains "Página de inicio" ' do
-            get '/'
-            expect(last_response.body).to include("Página de inicio")
-          end
+    it ' the page´s body should contains "Página de inicio" ' do
+      get '/'
+      expect(last_response.body).to include("Página de inicio")
+    end
 
-          it 'should return an exception because this page don´t redirect another page' do
-            get '/'
-            expect(lambda{follow_redirect!}).to raise_error(Rack::Test::Error, 'Last response was not a redirect. Cannot follow_redirect!')
-          end
+    it 'should return an exception because this page don´t redirect another page' do
+      get '/'
+      expect(lambda{follow_redirect!}).to raise_error(Rack::Test::Error, 'Last response was not a redirect. Cannot follow_redirect!')
+    end
 
-        end
+  end
