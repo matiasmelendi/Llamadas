5e3938e2327b90ecf625e47736eab46a62144d72
Matias <mmelendi@10pines.com>
2014-11-25 15:28:55 -0300

    method "se_aplica_a(llamada)" fixed

--- lib/restricciones/restriccion_por_zona.rb
+++ lib/restricciones/restriccion_por_zona.rb
@@ -3,7 +3,7 @@ require_relative 'restriccion_de_facturacion'
 class RestriccionPorZona < RestriccionDeFacturacion
 
   def se_aplica_a(llamada)
-    country_codes.include?(llamada.cod_nacional) && es_llamada_internacional(llamada)
+    country_codes.include?(llamada.cod_area_receptor.cod_nacional) && es_llamada_internacional(llamada)
   end
 
   private
