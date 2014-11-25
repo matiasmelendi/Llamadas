03b7b748890348b4da9f0c5ec4dd30cc025b2462
Matias <mmelendi@10pines.com>
2014-11-25 15:58:25 -0300

    tests for "facturar_mes" added

--- lib/test/facturador_de_llamadas_spec.rb
+++ lib/test/facturador_de_llamadas_spec.rb
@@ -7,9 +7,9 @@ describe 'El comportamiento del facturador de llamadas' do
   before do
     @compania_telefonica= CompaniaTelefonica.new(initialize)
     @compania_telefonica.agregar_cliente("Memo",LineaTelefonica.new(CodArea.new(120,54),1511111111),1)
-    @compania_telefonica.agregar_cliente("EuroMemo",LineaTelefonica.new(CodArea.new(1,54),1522222222),2)
+    @compania_telefonica.agregar_cliente("LocalMemo",LineaTelefonica.new(CodArea.new(1,54),1522222222),2)
     @memo= @compania_telefonica.cliente_de_nombre("Memo")
-    @euro_memo= @compania_telefonica.cliente_de_nombre("EuroMemo")
+    @local_memo= @compania_telefonica.cliente_de_nombre("LocalMemo")
     @registro= RegistroDeLlamadas.new
     @facturador= FacturadorDeLlamadas.new(@registro)
     @calendar=Calendario.new
@@ -25,13 +25,13 @@ describe 'El comportamiento del facturador de llamadas' do
     it 'si es nacional, en dia de semana y hora pico, su costo debería ser 50 cents por minuto' do
       @calendar.date_today = DateTime.new(2014,11,3,19,0,0)
       #nacional=30 cents, dia habil hora pico= 20 cents
-      @facturador.costo(Llamada.new(@memo,@euro_memo,Duration.new(1.to_minutes),@calendar.date_today)).should be(0.50.to_pesos)
+      @facturador.costo(Llamada.new(@memo,@local_memo,Duration.new(1.to_minutes),@calendar.date_today)).should be(0.50.to_pesos)
     end
 
     it 'si es nacional, en fin de semana, su costo debería ser 50 cents por minuto' do
       @calendar.date_today = DateTime.new(2014,11,1,21,0,0)
       #nacional=30 cents, fin de semana= 10 cents, hora no pico= 10 cents
-      @facturador.costo(Llamada.new(@memo,@euro_memo,Duration.new(1.to_minutes),@calendar.date_today)).should be(0.50.to_pesos)
+      @facturador.costo(Llamada.new(@memo,@local_memo,Duration.new(1.to_minutes),@calendar.date_today)).should be(0.50.to_pesos)
     end
 
     it 'si es local, en dia de semana y hora pico, su costo debería ser 20 cents por minuto' do
@@ -87,6 +87,24 @@ describe 'El comportamiento del facturador de llamadas' do
     end
 
 
+  end
+
+  context 'Cuando genera la factura para un cliente en un determinado mes' do
+
+    it 'Si un cliente no realiza llamadas, solo deberia facturarse el costo fijo' do
+      @factura=@compania_telefonica.facturar_mes(MesDelAnio.septiembre(2014),@memo)
+      @factura.monto_a_pagar.should equal(10.to_pesos)
+    end
+
+    it 'Si un cliente realiza una llamada en todo el mes, la factura mensual deberia cobrarle el valor de la llamada + el costo fijo' do
+      @calendar.date_today= DateTime.new(2014,11,11,18,0,0)
+      @compania_telefonica.agregar_cliente("cliente_europeo",LineaTelefonica.new(CodArea.new(101,102),1533333333),4)
+      cliente_europeo= @compania_telefonica.cliente_de_nombre("cliente_europeo")
+      @memo.realizar_llamada(Duration.new(10.to_minutes),cliente_europeo,@calendar.date_today)
+      @factura= @compania_telefonica.facturar_mes(MesDelAnio.noviembre(2014),@memo)
+      #0.20 por minuto hora pico y dia habil + 0.70 * 10 minutos a Europa + costo fijo
+      @factura.monto_a_pagar.should equal(19.00.to_pesos)
+    end
   end
 
 end
