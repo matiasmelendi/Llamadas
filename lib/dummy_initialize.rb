bc350d12143816b76df5e034e63433cadc143849
Matias <mmelendi@10pines.com>
2014-11-25 16:13:19 -0300

    fixed some methods

--- lib/dummy_initialize.rb
+++ lib/dummy_initialize.rb
@@ -3,21 +3,22 @@ require_relative 'cod_area'
 require_relative 'cliente'
 require_relative 'util/utilities'
 require_relative 'compania_telefonica'
-
+require_relative 'util/calendario'
 
 class DummyInitialize
   attr_accessor :compania
 
   def initialize
     @compania= CompaniaTelefonica.new([])
+    @calendar= Calendario.new
     compania.agregar_cliente("Memo",LineaTelefonica.new(CodArea.new(120,54),1511111111),1)
     compania.agregar_cliente("EuroMemo",LineaTelefonica.new(CodArea.new(1,101),1522222222),2)
-    compania.cliente_realizo_llamada_a(compania.cliente_de_nombre("Memo"),compania.cliente_de_nombre("EuroMemo"),Duration.new(10))
-    compania.cliente_realizo_llamada_a(compania.cliente_de_nombre("EuroMemo"),compania.cliente_de_nombre("Memo"),Duration.new(10))
+    compania.cliente_realizo_llamada_a(compania.cliente_de_nombre("Memo"),compania.cliente_de_nombre("EuroMemo"),Duration.new(10.to_minutes),@calendar.date_today)
+    compania.cliente_realizo_llamada_a(compania.cliente_de_nombre("EuroMemo"),compania.cliente_de_nombre("Memo"),Duration.new(10.to_minutes),@calendar.date_today)
  end
 
   def meses_corrientes
-    @date= Date.today
+    @date= @calendar.date_today
     anho= @date.year
     mes= @date.month
     meses= (1..13).collect{|nro_mes| MesDelAnio.new(anho,nro_mes)}
@@ -25,7 +26,7 @@ class DummyInitialize
   end
 
   def mes_del_anho_actual
-    @date= Date.today
+    @date= @calendar.date_today
     anho= @date.year
     mes= @date.month
     MesDelAnio.new(anho,mes)
