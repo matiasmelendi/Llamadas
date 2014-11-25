4e4ba163034a4f4e742a7ff699a8a51f5506e7a3
Matias <mmelendi@10pines.com>
2014-11-25 15:02:16 -0300

    "facturacion horas no pico" added

--- lib/restricciones/restriccion_de_facturacion.rb
+++ lib/restricciones/restriccion_de_facturacion.rb
@@ -18,21 +18,23 @@ class << self
           LlamandoAEuropa.new,LlamandoANorteAmerica.new,LlamandoASudamerica.new,
           RestriccionFinDeSemana.new,SinRestriccionPorZona.new,
           RestriccionDiaHabilEnHoraPico.new(RangoHorario.new(8.to_hours,20.to_hours)),
-          LlamadaLocal.new,LlamadaNacional.new
+          LlamadaLocal.new,LlamadaNacional.new, RestriccionHoraria.new(RangoHorario.new(20.to_hours,24.to_hours)),
+          RestriccionHoraria.new(RangoHorario.new(0.to_hours,8.to_hours))
       ]
-    end
-
-    def facturaciones_aplicadas(llamada)
-      tipos_de_facturacion.select{|facturacion| facturacion.se_aplica_a(llamada)}
-    end
-
-    def costo_de_facturaciones(facturaciones)
-      facturaciones.inject(0.to_pesos) { |result,facturacion | result + facturacion.costo}
     end
 
     def costo_para(llamada)
       costo_de_facturaciones(facturaciones_aplicadas(llamada)) * llamada.duracion
     end
 
-  end
+private
+    def facturaciones_aplicadas(llamada)
+     tipos_de_facturacion.select{|facturacion| facturacion.se_aplica_a(llamada)}
+    end
+
+    def costo_de_facturaciones(facturaciones)
+      facturaciones.inject(0.to_pesos) { |result,facturacion | result + facturacion.costo}
+    end
+
+end
 end
