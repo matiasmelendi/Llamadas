3a617746f15ff569ad1db9fae87af6c7f24988bd
Matias <mmelendi@10pines.com>
2014-11-25 15:41:03 -0300

    tests for international calls added

--- lib/test/facturador_de_llamadas_spec.rb
+++ lib/test/facturador_de_llamadas_spec.rb
@@ -20,7 +20,7 @@ describe 'El comportamiento del facturador de llamadas' do
     @compania_telefonica.eliminar_llamadas
   end
 
-  context 'Si se realiza una llamada' do
+  context 'Si se realiza una llamada nacional o local' do
 
     it 'si es nacional, en dia de semana y hora pico, su costo debería ser 50 cents por minuto' do
       @calendar.date_today = DateTime.new(2014,11,3,19,0,0)
@@ -48,6 +48,42 @@ describe 'El comportamiento del facturador de llamadas' do
       cliente_local= @compania_telefonica.cliente_de_nombre("cliente_local")
       #local=0 cents, hora no pico= 10 cents
       @facturador.costo(Llamada.new(@memo,cliente_local,Duration.new(1.to_minutes),@calendar.date_today)).should be(0.10.to_pesos)
+    end
+
+  end
+
+  context 'si realizo una llamada internacional' do
+
+    it 'si es a europa, hora no pico, su costo debería ser 80 cents por minuto' do
+      @calendar.date_today = DateTime.new(2014,11,3,21,0,0)
+      @compania_telefonica.agregar_cliente("cliente_europeo",LineaTelefonica.new(CodArea.new(101,102),1533333333),4)
+      cliente_europeo= @compania_telefonica.cliente_de_nombre("cliente_europeo")
+      #europa=70 cents, hora no pico= 10 cents
+      @facturador.costo(Llamada.new(@memo,cliente_europeo,Duration.new(1.to_minutes),@calendar.date_today)).should be(0.80.to_pesos)
+    end
+
+    it 'si es a sudamerica, hora no pico, su costo debería ser 60 cents por minuto' do
+      @calendar.date_today = DateTime.new(2014,11,3,21,0,0)
+      @compania_telefonica.agregar_cliente("cliente_sudamericano",LineaTelefonica.new(CodArea.new(50,51),1533333333),5)
+      cliente_sudamericano= @compania_telefonica.cliente_de_nombre("cliente_sudamericano")
+      #sudamerica=50 cents, hora no pico= 10 cents
+      @facturador.costo(Llamada.new(@memo,cliente_sudamericano,Duration.new(1.to_minutes),@calendar.date_today)).should be(0.60.to_pesos)
+    end
+
+    it 'si es a norte america, hora no pico, su costo debería ser X0 cents por minuto' do
+      @calendar.date_today = DateTime.new(2014,11,3,21,0,0)
+      @compania_telefonica.agregar_cliente("cliente_norteamericano",LineaTelefonica.new(CodArea.new(10,11),1533333333),6)
+      cliente_norteamericano= @compania_telefonica.cliente_de_nombre("cliente_norteamericano")
+      #norte america=70 cents, hora no pico= 10 cents
+      @facturador.costo(Llamada.new(@memo,cliente_norteamericano,Duration.new(1.to_minutes),@calendar.date_today)).should be(0.80.to_pesos)
+    end
+
+    it 'si es a cualquier otro lugar del mundo no especificado, hora no pico, su costo debería ser X0 cents por minuto' do
+      @calendar.date_today = DateTime.new(2014,11,3,21,0,0)
+      @compania_telefonica.agregar_cliente("cliente_X",LineaTelefonica.new(CodArea.new(1001,1002),1533333333),7)
+      cliente_X= @compania_telefonica.cliente_de_nombre("cliente_X")
+      #resto del mundo= 1.50 pesos, hora no pico= 10 cents
+      @facturador.costo(Llamada.new(@memo,cliente_X,Duration.new(1.to_minutes),@calendar.date_today)).should be(1.60.to_pesos)
     end
 
 
